df11bf5ffd8b2abf0fc41cb864f8eaa820306b78
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2023-01-14 23:33:37 +0100

    draw character and enemies

--- js/classes/world.class.js
+++ js/classes/world.class.js
@@ -5,15 +5,27 @@ class World {
         new JellyFish(),
         new BigBoss(),
     ];
+    canvas;
     ctx;
 
     constructor(canvas){
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas
         this.draw();
     }
 
 
     draw(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        this.enemies.forEach(enemyFish => {
+            this.ctx.drawImage(enemyFish.img, enemyFish.x, enemyFish.y, enemyFish.width, enemyFish.height);
+        });
+
+            let self = this;
+            requestAnimationFrame(function(){
+                self.draw();
+            });
     }
 }
