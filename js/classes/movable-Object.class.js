21e830f03668d9c1588c0fb85171de7fbe655557
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2023-01-14 01:42:19 +0100

    draw character

--- js/classes/movable-Object.class.js
+++ js/classes/movable-Object.class.js
@@ -1,31 +1,29 @@
 class MovableObjekt {
-    x;
-    y;
+    x = 20;
+    y = 300;
     img;
+    height = 150;
+    width = 150;
 
 
     loadImage(path){
         this.img = new Image();
-        this.img.src = path
+        this.img.src = path;
 
     }
 
     moveRight(){
-
     }
 
 
     moveLeft(){
-
     }
 
 
     moveUp(){
-
     }
 
 
     moveDown(){
-        
     }
 }
