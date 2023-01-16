bf314f785207f41c181f97692a7df0b2a70fc1eb
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2023-01-16 20:43:53 +0100

    animate Character

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -1,9 +1,43 @@
 class Character extends MovableObject {
+    Images_Idle = [
+        './img/1.Sharkie/1.IDLE/1.png',
+        './img/1.Sharkie/1.IDLE/2.png',
+        './img/1.Sharkie/1.IDLE/3.png',
+        './img/1.Sharkie/1.IDLE/4.png',
+        './img/1.Sharkie/1.IDLE/5.png',
+        './img/1.Sharkie/1.IDLE/6.png',
+        './img/1.Sharkie/1.IDLE/7.png',
+        './img/1.Sharkie/1.IDLE/8.png',
+        './img/1.Sharkie/1.IDLE/9.png',
+        './img/1.Sharkie/1.IDLE/10.png',
+        './img/1.Sharkie/1.IDLE/11.png',
+        './img/1.Sharkie/1.IDLE/12.png',
+        './img/1.Sharkie/1.IDLE/13.png',
+        './img/1.Sharkie/1.IDLE/14.png',
+        './img/1.Sharkie/1.IDLE/15.png',
+        './img/1.Sharkie/1.IDLE/16.png',
+        './img/1.Sharkie/1.IDLE/17.png',
+        './img/1.Sharkie/1.IDLE/18.png',
+    ];
+    currentImage = 0;
+
     constructor(){
         super().loadImage('./img/1.Sharkie/1.IDLE/1.png');
+        this.loadImages(this.Images_Idle);
+
+        this.animate();
     }
 
-    attack(){
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.Images_Idle.length;
+            let path = this.Images_Idle[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 99);
     }
+
+    // attack(){
+    // }
 
 }
