5205f398e41e4dae4acfebec256d4256acadf726
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2023-01-19 23:40:03 +0100

    optimize code Structure

---
+++ js/models/level.class.js
@@ -0,0 +1,13 @@
+class Level {
+    enemies;
+    ground;
+    water;
+    bgShadow;
+
+    constructor(enemies, ground, water, bgShadow){
+        this.enemies = enemies;
+        this.ground = ground;
+        this.water = water;
+        this.bgShadow = bgShadow;
+    }
+}
--- js/models/world.class.js
+++ js/models/world.class.js
@@ -1,30 +1,11 @@
 class World {
     character = new Character();
-    enemies = [new PufferFish(), new JellyFish(), new BigBoss(),];
+    enemies = level1.enemies;
     lights = [new Light(),];
     barriers = [new BarrierBlock(),];
-    ground = [
-        new Floor('./img/3. Background/Layers/2. Floor/L2.png', -1100, 180),
-        new Floor('./img/3. Background/Layers/2. Floor/L1.png', 0, 180),
-        new Floor('./img/3. Background/Layers/2. Floor/L2.png', 1100, 180),
-        new Floor('./img/3. Background/Layers/2. Floor/L1.png', 2200, 180),
-    ];
-    water = [
-        new Water('./img/3. Background/Layers/5. Water/L2.png', -1100),
-        new Water('./img/3. Background/Layers/5. Water/L1.png', 0),
-        new Water('./img/3. Background/Layers/5. Water/L2.png', 1100),
-        new Water('./img/3. Background/Layers/5. Water/L1.png', 2200),
-    ];
-    bgShadow = [
-        new BackgroundObject('./img/3. Background/Legacy/Layers/4.Fondo 2/L2.png', -1100),
-        new BackgroundObject('./img/3. Background/Legacy/Layers/3.Fondo 1/L2.png', -1100),
-        new BackgroundObject('./img/3. Background/Legacy/Layers/4.Fondo 2/L1.png', 0),
-        new BackgroundObject('./img/3. Background/Legacy/Layers/3.Fondo 1/L1.png', 0),
-        new BackgroundObject('./img/3. Background/Legacy/Layers/4.Fondo 2/L2.png', 1100),
-        new BackgroundObject('./img/3. Background/Legacy/Layers/3.Fondo 1/L2.png', 1100),
-        new BackgroundObject('./img/3. Background/Legacy/Layers/4.Fondo 2/L1.png', 2200),
-        new BackgroundObject('./img/3. Background/Legacy/Layers/3.Fondo 1/L2.png', 2200),
-    ];
+    ground = level1.ground;
+    water = level1.water
+    bgShadow = level1.bgShadow;
     canvas;
     ctx;
     keyboard;
