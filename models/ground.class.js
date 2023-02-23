e16eb902abaebb45dc924cee29f1ce9cdfb9c741
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2023-02-23 16:18:09 +0100

    update2 calculate floor coordinates

--- models/ground.class.js
+++ models/ground.class.js
@@ -5,7 +5,7 @@ class Floor extends MovableObject {
     constructor(imagePath, x, y){
         super().loadImage(imagePath);
         this.x = x;
-        this.y = y;
+        this.y = 768 - this.height; // y-koordinate berechnen indem die height (vom Object 500) von der vergebenen canvas höhe (768) abgezogen wird. So kann das Objekt an der unteren stelle vom Canvas positioniert werden, ohne mehrmals zu korrigieren oder anzupassen.
     }
     
 }
