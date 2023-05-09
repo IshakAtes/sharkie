1fcd1369a38a811472e0e6483ceccfd7843e3fb2
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2023-05-09 14:56:02 +0200

    toggle speakerIcon

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
+let gameStatement = false;
 function startGame() {
     let startScreen = document.getElementById('startCtnId');
     startScreen.style.display = 'none';
+    gameStatement = true;
     init();
 }
 
 
 function toggleSound() {
+    let speakerPath = document.getElementById('speakerId');
     if (audioOn) {
         audioOn = false;
         background_sound.pause();
+        speakerPath.src = "./img/speakerIcon/stumm (2).png";
     } else {
+        if (gameStatement) {
+            background_sound.play();
+            background_sound.loop = true
+        }
         audioOn = true;
-        background_sound.play();
-        background_sound.loop = true
+        speakerPath.src = "./img/speakerIcon/mittleres-volumen.png";
     }
 }
