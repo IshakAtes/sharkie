3e8482823521f380497e4912460bfc2438eef762
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2023-11-02 13:54:01 +0100

    next Step save the tryAgainStatement in local storage

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 let gameStatement = false;
+let tryAgainStatement = false;
+
+
+function checkAndReplay(){
+    console.log('Replay', tryAgainStatement);
+    if (tryAgainStatement) {
+        startGame();
+    }
+}
+
+
+function tryAgain() {
+    tryAgainStatement = true;
+    console.log(tryAgainStatement);
+    setTimeout(() => {
+        window.location.href = 'index.html';
+    }, 1000);
+}
 
 
 function startGame() {
+    tryAgainStatement = false;
     let startScreen = document.getElementById('startCtnId');
     startScreen.style.display = 'none';
     gameStatement = true;
