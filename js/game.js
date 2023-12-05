703eba2bc6b2393e37c360e1b5d7b27b2a9594f4
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2023-12-05 23:38:54 +0100

    add some js doc comments part 6

--- js/game.js
+++ js/game.js
@@ -5,12 +5,18 @@ let background_sound = new Audio('./sounds/underwater.mp3');
 let audioOn = false;
 
 
+/**
+ * This function load the Game
+ */
 function init(){
     canvas = document.getElementById('canvasId');
     backgroundAudio();
     world = new World(canvas, keyboard);
 }
 
+/**
+ * This function check the audioOn boolean and place the correct speaker image in the ControllPanel area
+ */
 function backgroundAudio() {
     if (audioOn == true) {
         let speaker = document.getElementById('speakerId');
@@ -26,80 +32,122 @@ function backgroundAudio() {
 }
 
 
-// MOVE CONTROLLER FOR SMARTPHONES
+/**
+ * Smartphone Start Move Up
+ */
 function moveUp() {
     keyboard.UP = true;
     document.getElementById('up').style.opacity = '0.7';
 }
 
+/**
+ * Smartphone Start Move Down
+ */
 function moveDown() {
     keyboard.DOWN = true;
     document.getElementById('down').style.opacity = '0.7';
 }
 
+/**
+ * Smartphone Start Move Right
+ */
 function moveRight() {
     keyboard.RIGHT = true;
     document.getElementById('right').style.opacity = '0.7';
 }
 
+/**
+ * Smartphone Start Move Left
+ */
 function moveLeft() {
     keyboard.LEFT = true;
     document.getElementById('left').style.opacity = '0.7';
 }
 
+/**
+ * Smartphone Start Move Slap
+ */
 function moveSlap() {
     keyboard.V = true;
     document.getElementById('slap').style.opacity = '0.8';
 }
 
+/**
+ * Smartphone Start Move Poisen Attack
+ */
 function movePoisenAtk() {
     keyboard.B = true;
     document.getElementById('poisenAttack').style.opacity = '0.8';
 }
 
+/**
+ * Smartphone Start Move Normal Bubble Attack
+ */
 function moveNormalAtk() {
     keyboard.SPACE = true;
     document.getElementById('normalBubble').style.opacity = '0.8';
 }
 
-
+/**
+ * Smartphone Stop Move Up
+ */
 function clearUp() {
     keyboard.UP = false;
     document.getElementById('up').style.opacity = '0.5';
 }
 
+/**
+ * Smartphone Stop Move Down
+ */
 function clearDown() {
     keyboard.DOWN = false;
     document.getElementById('down').style.opacity = '0.5';
 }
 
+/**
+ * Smartphone Stop Move Right
+ */
 function clearRight() {
     keyboard.RIGHT = false;
     document.getElementById('right').style.opacity = '0.5';
 }
 
+/**
+ * Smartphone Stop Move Left
+ */
 function clearLeft() {
     keyboard.LEFT = false;
     document.getElementById('left').style.opacity = '0.5';
 }
 
+/**
+ * Smartphone Stop Move Slap
+ */
 function clearSlap() {
     keyboard.V = false;
     document.getElementById('slap').style.opacity = '0.6';
 }
 
+/**
+ * Smartphone Stop Move Poisen Attack
+ */
 function clearPoisenAtk() {
     keyboard.B = false;
     document.getElementById('poisenAttack').style.opacity = '0.6';
 }
 
+/**
+ * Smartphone Stop Move Normal Bubble Attack
+ */
 function clearNormalAtk() {
     keyboard.SPACE = false;
     document.getElementById('normalBubble').style.opacity = '0.6';
 }
 
 
-
+/**
+ * Check which keys are pressed
+ */
 window.addEventListener('keydown', (event) => {
     if (event.keyCode == 39) {
         keyboard.RIGHT = true;
@@ -132,7 +180,9 @@ window.addEventListener('keydown', (event) => {
     }
 });
 
-
+/**
+ * Check which keys you let off
+ */
 window.addEventListener('keyup', (event) => {
     if (event.keyCode == 39) {
         keyboard.RIGHT = false;
